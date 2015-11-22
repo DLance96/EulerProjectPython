739419efdca0961e5cc1cf965883b05c6f9cff95
David Lance <dcl71@case.edu>
2015-11-22 00:34:40 -0500

    solved problem 1

--- prob1.py
+++ prob1.py
@@ -1,9 +1,16 @@
 # Sum the multiples of 3 and 5 between 1 and 1000
 
-def make_mult_3_5_list()
-    
+def make_3_5_list(upper):
+    mult_list = []
+    for x in range(0, upper):
+        if (x % 3 == 0 or x % 5 == 0):
+            mult_list.append(x)
     return mult_list
 
-def sum_list(list)
-
+def sum_list(alist):
+    sum = 0
+    for x in alist:
+        sum = sum + x
     return sum
+
+print ( sum_list( make_3_5_list(1000) ) ) 
